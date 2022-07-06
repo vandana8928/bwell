5dc3a1569bbba92ebe4610edcf34c63494741ef6
Vandana Yadav <86240580+vandana8928@users.noreply.github.com>
2022-07-06 14:28:34 +0530

    Add files via upload

---
+++ B_well/ptsd/js/questions.js
@@ -0,0 +1,36 @@
+let questions = [
+  {
+    numb: 1,
+    question: "had nightmares about the event(s) or thought about the event(s) when you did not want to?",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 2,
+    question: "tried hard not to think about the event(s) or went out of your way to avoid situations that reminded you of the event(s)?",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 3,
+    question:
+      " been constantly on guard, watchful, or easily startled?",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 4,
+    question: " felt dumb or detached from people, activities, or your surroundings?",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 5,
+    question: "felt guilty or unable to stop blaming yourself or others for the event(s) or any problems the event(s) may have caused?",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  
+];
