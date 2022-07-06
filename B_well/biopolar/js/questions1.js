d05d97a19265820d437dec15c3b5d13ec0a5dba0
Vandana Yadav <86240580+vandana8928@users.noreply.github.com>
2022-07-06 14:20:13 +0530

    Add files via upload

---
+++ B_well/biopolar/js/questions1.js
@@ -0,0 +1,53 @@
+let questions = [
+  {
+    numb: 1,
+    question: " Have you exercised today or yesterday?",
+    answer: "yes",
+    options: ["yes", "no", "maybe"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+  {
+    numb: 1,
+    answer: "yes",
+    options: ["yes", "no"],
+  },
+];
