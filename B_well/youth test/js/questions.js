55cacd079611765636c7c05a3d7e3f9a1075ba48
Vandana Yadav <86240580+vandana8928@users.noreply.github.com>
2022-07-06 14:30:37 +0530

    Add files via upload

---
+++ B_well/youth test/js/questions.js
@@ -0,0 +1,65 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Complain of aches or pains",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 2,
+    question: "Spend more time alone",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 3,
+    question: "Tire easily, little energy",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 4,
+    question: "Fidgety, unable to sit still",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 5,
+    question: " Have trouble with teacher",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 6,
+    question: "Less interested in school",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 7,
+    question: "Daydream too much",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 8,
+    question: "Distract easily",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+  {
+    numb: 9,
+    question: "Are afraid of new situations",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+
+  {
+    numb: 10,
+    question: "Feel hopeless",
+    answer: "No",
+    options: ["Yes", "No"],
+  },
+];
